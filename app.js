403d34e0ce8dbab04f8a08ae9a5fe2a30f8c2431
chidrome <chidrome@gmail.com>
2019-01-07 08:41:49 -0800

    hw turn in

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 // TODOS
 // 1. Require bcrypt
+var readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ', {
+}); 
+console.log('Hi ' + userName + '!');
+
 // 2. Get a password from the user
+var userPassword = readlineSync.question('Please enter your password: ', {
+	hideEchoBack: true
+});
+
 // 3. Hash the password and store result in a local variable
+var bcrypt = require('bcrypt');
+var saltRounds = 10;
+var hashedPassword = bcrypt.hashSync(userPassword, saltRounds);
+
 // 4. Get another password from the user
+var userPasswordConfirmation = readlineSync.question('Please confirm your password: ', {
+	hideEchoBack: true
+});
+var passwordConfirmationCheck = bcrypt.compareSync(userPasswordConfirmation, hashedPassword)
+
 // 5. If the second password matches the first one, print a success message
 // 6. If the second password does not match the first one, print 'access denied'
+if(passwordConfirmationCheck){
+	console.log('Password Accepted');
+}
+else if(!passwordConfirmationCheck){
+	console.log('Access Denied')
+}
