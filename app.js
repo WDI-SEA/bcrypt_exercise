9f20401be8e1ed525c5270c30dbf2bfdacdc881d
mtom92 <torresmoranm@gmail.com>
2019-04-11 23:55:26 -0700

    working and password hidden

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ var someOtherPlaintextPassword
 var userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');
 // 2. Get a password from the user
-myPlaintextPassword= readlineSync.question('May I have your password? ');
+myPlaintextPassword= readlineSync.question('May I have your password? ',{
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+})
 // 3. Hash the password and store result in a local variable
 var hashedPassword = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 // 4. Get another password from the user
-someOtherPlaintextPassword = readlineSync.question('Please confirm your password ');
+someOtherPlaintextPassword = readlineSync.question('Please confirm your password ',{
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+})
+
 var result = bcrypt.compareSync(someOtherPlaintextPassword, hashedPassword);
 // 5. If the second password matches the first one, print a success message
 result ? console.log('acces approved') : console.log('acces denied');
