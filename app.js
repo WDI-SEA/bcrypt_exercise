dec8508a0ef5072548597cbcef5795e2d7e13e64
yboodhan <yboodhan@gmail.com>
2019-12-17 11:05:29 -0800

    entered answers

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-// TODOS
-// 1. Require bcrypt
-// 2. Get a password from the user
-// 3. Hash the password and store result in a local variable
-// 4. Get another password from the user
-// 5. If the second password matches the first one, print a success message
-// 6. If the second password does not match the first one, print 'access denied'
+// Ask the user for a name
+// Print "hi" + user's name
+// Ask the user for a password
+// Hash the password the user gives and store it in a local variable
+// Ask the user to type in their password again
+// Compare the hashed password to the retyped in password.
+// Depending on whether the user typed in the same password or a different one, print out an appropriate message.
+
+var readlineSync = require('readline-sync');
+var bcrypt = require('bcryptjs');
+var saltRounds = 10;
+
+
+var userName = readlineSync.question('May I have your name? ');
+
+console.log('Hi ' + userName + '!');
+
+var password = readlineSync.question('Set a password ',
+    {hideEchoBack: true}
+);
+
+var hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+var verifyPassword = readlineSync.question('Verify your password ',
+    {hideEchoBack: true}
+);
+
+var result = bcrypt.compareSync(verifyPassword, hashedPassword);
+
+if (result) {
+    console.log('You verified your password correctly! Welcome!')
+} else {
+    console.log('Your password is incorrect! Access denied!')
+}
