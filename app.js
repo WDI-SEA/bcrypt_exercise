76d4985b345ef7987d7faae1e9d5c0607458bba6
davidvelichko52 <davidvelichko52@gmail.com>
2020-04-10 00:25:39 -0700

    done

--- app.js
+++ app.js
@@ -5,3 +5,36 @@
 // 4. Get another password from the user
 // 5. If the second password matches the first one, print a success message
 // 6. If the second password does not match the first one, print 'access denied'
+
+
+var readlineSync = require('readline-sync');
+var bcrypt = require('bcryptjs');
+var saltRounds = 10;
+var myPlaintextPassword = 'password';
+var someOtherPlaintextPassword = 'not_password';
+
+
+
+var userName = readlineSync.question('May I have your username? ');
+console.log('Hi ' + userName + '!');
+
+var secretCode = readlineSync.question('What is your Code ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+
+var secretCodeTwo = readlineSync.question('confirm your password ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+
+
+var hashedPassword = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+
+var result = bcrypt.compareSync(myPlaintextPassword, hashedPassword);
+
+
+  if(secretCode == secretCodeTwo) {
+    console.log('You passed ' + userName)
+  }else {
+    console.log('I am calling the cops!')
+  }
+
