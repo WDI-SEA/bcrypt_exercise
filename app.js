97a31683750ad6aecdef2619c77439d1a9aa37d4
Chuck Wagon <chuckwagon@Chucks-MacBook-Air.local>
2020-04-09 22:04:26 -0700

    are you stealing my password?

--- app.js
+++ app.js
@@ -5,3 +5,26 @@
 // 4. Get another password from the user
 // 5. If the second password matches the first one, print a success message
 // 6. If the second password does not match the first one, print 'access denied'
+
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+var favFood = readlineSync.question('What is your favorite food? ', {
+    hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+  });
+  console.log('Oh, you love ' + favFood + '!');
+
+var bcrypt = require('bcryptjs');
+var saltRounds = 2;
+var myPlaintextPassword = 'password';
+var someOtherPlaintextPassword = 'not_password';
+
+var hashedPassword = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+
+// Print it out
+console.log('Hashed password is', hashedPassword);
+
+var result = bcrypt.compareSync(myPlaintextPassword, hashedPassword); 
+var result = bcrypt.compareSync(someOtherPlaintextPassword, hashedPassword); 
